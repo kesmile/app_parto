91115454a335775803ff5c0045d0f9bc699335e2
kelvi padilla <kurtkelvisk88@gmail.com>
2016-01-06 21:11:39 -0600

    build android app

--- www/js/controller.js
+++ www/js/controller.js
@@ -9,7 +9,7 @@ angular.module('starter')
 
     $http({
          method: 'POST',
-         url: 'http://partomaya.herokuapp.com/login/validate_token',
+         url: 'http://fotoscomadronas.herokuapp.com/login/validate_token',
          data: 'token=' + form.token,
          headers: {'Content-Type': 'application/x-www-form-urlencoded'}
        }).success(function(data, status, headers, config) {
@@ -27,11 +27,22 @@ angular.module('starter')
             });
             $scope.loading = false;
          }
-       });
+       }).error(function (data, status) {
+         console.log(status);
+         var alertPopup = $ionicPopup.alert({
+            title: 'Error',
+            template: data
+          });
+
+          alertPopup.then(function(res) {
+
+          });
+          $scope.loading = false;
+        });
   }
 });
 angular.module('starter')
-.controller('HomeCtrl',function($scope, $http, $location,$ionicPopup,TokenServices){
+.controller('HomeCtrl',function($scope, $http, $location,$ionicPopup,TokenServices,$cordovaToast){
   $scope.popUp = function(tipo){
     var confirmPopup = $ionicPopup.confirm({
      title: 'Enviar notificacion',
@@ -42,11 +53,17 @@ angular.module('starter')
      if(res) {
        $http({
             method: 'POST',
-            url: 'http://partomaya.herokuapp.com/dashboard/add_evento',
+            url: 'http://fotoscomadronas.herokuapp.com/dashboard/add_evento',
             data: 'token=' + TokenServices.getToken() +'&tipo=' + tipo,
             headers: {'Content-Type': 'application/x-www-form-urlencoded'}
           }).success(function(data, status, headers, config) {
-            console.log(data);
+            $cordovaToast
+              .show('¡Mensaje enviado exitosamente!', 'long', 'center')
+              .then(function(success) {
+                // success
+              }, function (error) {
+                // error
+              });
           });
      }
    });
